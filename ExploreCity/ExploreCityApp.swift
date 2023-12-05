3327c49ae2a2036d29a63123e552e14709d6b9a4
rojinpra7 <89182903+rojinpra7@users.noreply.github.com>
2023-12-05 12:01:05 -0500

    Initial Commit

---
+++ ExploreCity/ExploreCityApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExploreCityApp.swift
+//  ExploreCity
+//
+//  Created by Rojin Prajapati on 12/5/23.
+//
+
+import SwiftUI
+
+@main
+struct ExploreCityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
